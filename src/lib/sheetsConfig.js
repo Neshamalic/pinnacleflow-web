db1fb9b6088070e58a4dfdee41b0ea4eea6e6c16
Neshamalic <212040406+Neshamalic@users.noreply.github.com>
2025-08-20 12:15:57 -0400

    Update sheetsConfig.js

--- src/lib/sheetsConfig.js
+++ src/lib/sheetsConfig.js
@@ -1,2 +1,2 @@
 export const SHEET_ID = "1Cm3xkr2dhk7I_apZByXDN_GHF3A-dYy1WnDEehIFan0";
-export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyQ9ZKQfcrwJpuJ8inHvFG7td0kwYrPcvtzhC-_s7XOCPOSgd9x4alFAVeD0jzXPIZd/exec';
+export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyKM_QMTMumPq6xvegLPFR_bfYGRuQOZ_sllBdPcl8UKxOQfPTobrJFfgfyyifYGcxN/exec';

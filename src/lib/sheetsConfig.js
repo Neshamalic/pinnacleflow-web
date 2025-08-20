4307cbdc9a83702bfeb051f99c22263b9fb6bfc2
Neshamalic <212040406+Neshamalic@users.noreply.github.com>
2025-08-20 14:23:16 -0400

    Update sheetsConfig.js

--- src/lib/sheetsConfig.js
+++ src/lib/sheetsConfig.js
@@ -1,2 +1,2 @@
 export const SHEET_ID = "1Cm3xkr2dhk7I_apZByXDN_GHF3A-dYy1WnDEehIFan0";
-export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyKM_QMTMumPq6xvegLPFR_bfYGRuQOZ_sllBdPcl8UKxOQfPTobrJFfgfyyifYGcxN/exec';
+export const APPS_SCRIPT_URL = 'https://script.google.com/a/macros/pinnaclelifescience.com/s/AKfycbyKM_QMTMumPq6xvegLPFR_bfYGRuQOZ_sllBdPcl8UKxOQfPTobrJFfgfyyifYGcxN/exec';

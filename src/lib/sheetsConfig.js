e897a802bdddd27cce30a8522e3de43e3d737c86
Neshamalic <212040406+Neshamalic@users.noreply.github.com>
2025-08-20 22:09:44 -0400

    Update sheetsConfig.js

--- src/lib/sheetsConfig.js
+++ src/lib/sheetsConfig.js
@@ -1,2 +1,6 @@
-export const SHEET_ID = "1Cm3xkr2dhk7I_apZByXDN_GHF3A-dYy1WnDEehIFan0";
-export const APPS_SCRIPT_URL = 'https://script.google.com/a/macros/pinnaclelifescience.com/s/AKfycbyKM_QMTMumPq6xvegLPFR_bfYGRuQOZ_sllBdPcl8UKxOQfPTobrJFfgfyyifYGcxN/exec';
+// src/lib/sheetsConfig.js
+// Mantén el SHEET_ID actual
+export const SHEET_ID = '1Cm3xkr2dhk7I_apZByXDN_GHF3A-dYy1WnDEehIFan0';
+
+// 🔴 Pega aquí tu URL de la web app (la que termina en /exec)
+export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwYcElKGqnujXO0Tbhbbknu-SaJHYoka8-HiRTRh1rDZEu_FpxuuOpjWctusNEkOGrn/exec';

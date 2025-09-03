0cfec892b7078ba318d8c40640b52bcb64cb1b60
Neshamalic <212040406+Neshamalic@users.noreply.github.com>
2025-09-03 17:15:19 -0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,7 @@ import App from "./App";
 
 // Tus estilos base
 import "./styles/tailwind.css";
-import "./styles/index.css";
 
-// >>> Piel Rocket, debe ir al final para sobrescribir <<<
-import "./styles/rocket-skin.css";
+
 
 createRoot(document.getElementById("root")).render(<App />);

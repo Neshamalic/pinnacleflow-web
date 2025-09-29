fcb8f6ce0d9f93a80580e8e1aa6ae50660b6f698
Neshamalic <212040406+Neshamalic@users.noreply.github.com>
2025-09-29 10:38:03 -0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,10 @@
-// src/index.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-
-// skin y estilos
-import "./styles/rocket-skin.css";
-import "./styles/index.css";     // ← este es el que faltaba
 import "./styles/tailwind.css";
+import "./styles/index.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+const container = document.getElementById("root");
+const root = createRoot(container);
 
+root.render(<App />);
